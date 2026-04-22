82b569b64d4ea1cc5d039920e3efa43ed35332f9
final0pay <dayronaguilarmeneces1@gmail.com>
2026-04-22 15:09:51 +0200

    sesion2

---
+++ InsertaImparteDesdeCSV.java
@@ -0,0 +1,11 @@
+package cursos;
+
+import java.sql.*;
+
+public class InsertaImparteDesdeCSV implements DataBaseTask {
+
+    @Override
+    public void run(Connection conn, String data) throws BBDDException, SQLException {
+    	
+    }
+}
